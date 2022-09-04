e2b3b531935ef71521eb031ae39ee5d85eb32fed
progali007 <code.progali@gmail.com>
2022-09-05 03:12:53 +0430

    Change Some styles

--- gamer-ranking-card/src/components/AwardIcon.jsx
+++ gamer-ranking-card/src/components/AwardIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AwardIcon = ({children, title, number}) => {
    return (
       <div className='flex flex-col items-center gap-1'>
-         <span className='text-white text-sm font-bold'>{title}</span>
+         <span className='text-white text-xs font-semibold'>{title}</span>
          <div className='text-white'>{children}</div>
          <span className='text-white text-base font-bold'>{number}</span>
       </div>
--- gamer-ranking-card/src/components/GameAward.jsx
+++ gamer-ranking-card/src/components/GameAward.jsx
@@ -5,9 +5,9 @@ import GamepadFillIcon from 'remixicon-react/GamepadFillIcon';
 import CupFillIcon from 'remixicon-react/CupFillIcon';
 import AwardIcon from './AwardIcon';
 
-const GameAward = () => {
+const GameAward = ({className}) => {
   return (
-    <div className='flex items-center justify-between'>
+    <div className={`flex items-center justify-between ${className}`}>
       <AwardIcon title={'Awards'} number={'2'}>
          <TrophyFillIcon />
       </AwardIcon>
