87b606a0b8f3f9a1bd51a60bc8d7fc4327646c28
progali007 <code.progali@gmail.com>
2022-09-05 03:09:07 +0430

    Designs Icons

---
+++ gamer-ranking-card/src/components/AwardIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AwardIcon = ({children, title, number}) => {
+   return (
+      <div className='flex flex-col items-center gap-1'>
+         <span className='text-white text-sm font-bold'>{title}</span>
+         <div className='text-white'>{children}</div>
+         <span className='text-white text-base font-bold'>{number}</span>
+      </div>
+   )
+}
+
+export default AwardIcon
---
+++ gamer-ranking-card/src/components/GameAward.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import TrophyFillIcon from 'remixicon-react/TrophyFillIcon';
+import GroupFillIcon from 'remixicon-react/GroupFillIcon';
+import GamepadFillIcon from 'remixicon-react/GamepadFillIcon';
+import CupFillIcon from 'remixicon-react/CupFillIcon';
+import AwardIcon from './AwardIcon';
+
+const GameAward = () => {
+  return (
+    <div className='flex items-center justify-between'>
+      <AwardIcon title={'Awards'} number={'2'}>
+         <TrophyFillIcon />
+      </AwardIcon>
+      <AwardIcon title={'Matches'} number={'27'}>
+         <GamepadFillIcon />
+      </AwardIcon>
+      <AwardIcon title={'PALS'} number={'147'}>
+         <GroupFillIcon />
+      </AwardIcon>
+      <AwardIcon title={'Awards'} number={'∞'}>
+         <CupFillIcon />
+      </AwardIcon>
+    </div>
+  )
+}
+
+export default GameAward
