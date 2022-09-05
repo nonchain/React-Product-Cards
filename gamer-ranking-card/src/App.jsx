063bfc5236036d83c88039f576537daeb803c4bb
progali007 <code.progali@gmail.com>
2022-09-05 04:37:25 +0430

    Add Components to App.jsx and design them

--- gamer-ranking-card/src/App.jsx
+++ gamer-ranking-card/src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import AlertFillIcon from 'remixicon-react/AlertFillIcon'
+import Card from './components/Card';
+import GamerStatus from './components/GamerStatus';
+import GamerSummary from './components/GamerSummary';
+
+import GamerBoyAvatar from './assets/image/gamer_boy.png';
+import GamerDetails from './components/GamerDetails';
 
 function App() {
-  return <AlertFillIcon className='text-red-400' size={32}/>
+  return (
+    <div className="content">
+      <Card className='grid grid-rows-3 relative card'>
+        <GamerSummary
+          fullName={'Richard Tyler Blevins'}
+          className='row-span-2'
+          summary={'Better known as Ninja, is an American Twitch streamer, YouTuber and professional gamer.'} />
+
+        <div className='w-full h-28 bg-red-500 absolute bottom-0 overflow-hidden additional'>
+          <GamerDetails
+            fullName={'Richard Tyler Blevins'}
+            group={'Fire Friends'}
+            role={'Gamer'}
+            joinDate={'2022/5'}
+            city={'Abhar'}
+            country={'Iran'}
+            className='absolute bottom-[108px]'
+          />
+
+          <GamerStatus avatarUrl={GamerBoyAvatar} level='13' score='5,321' className='w-full absolute bottom-6' />
+        </div>
+
+      </Card>
+
+    </div>
+  )
 }
 
 export default App
