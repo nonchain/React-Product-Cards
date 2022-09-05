1be1436c6c777b1a4cc1c8896770042a7f273e7e
progali007 <code.progali@gmail.com>
2022-09-05 14:35:53 +0430

    Add functionality to show more button

--- news-cards/src/App.jsx
+++ news-cards/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card';
 import CardImage from './components/CardImage';
 import CardHeader from './components/CardHeader';
@@ -9,28 +9,30 @@ import Boxing from './assets/images/boxing.jpg';
 import CardAction from './components/CardAction';
 
 function App() {
+  const [actionsVisibility, setActionsVisibility] = useState(true);
+
+  const actionsVisibilityHandler = ()=>{
+    setActionsVisibility(!actionsVisibility)
+  }
+
   return (
 
     <div className="content">
-      <Card>
-        <CardHeader className={'pt-4 pb-3 px-3 bg-slate-400 flex flex-col items-center'}>
-          <span className='text-2xl font-bold relative day'>12</span>
-          <span className='text-base font-normal'>AUG</span>
-          <span className='text-base font-normal'>2022</span>
-        </CardHeader>
+      <Card className={'card'}>
+        <CardHeader />
         <CardImage src={Boxing} />
 
-        <div className='flex flex-col absolute bottom-0 left-0 z-20'>
-          <CardAction iconSize={24} comment={'8'} like={'47'} save={'0'}/>
+        <div className='flex flex-col absolute bottom-0 left-0 z-20 translate-y-24 details'>
+          <CardAction iconSize={24} comment={'8'} like={'47'} save={'0'} className={`z-10 ${actionsVisibility && 'translate-y-full'} `}/>
 
           <CardFooter
-            className={'pt-4 pb-5 px-5 bg-white relative'}
+            className={'pt-4 pb-5 px-5 bg-white relative z-20'}
             author={'Jane Doe'}
             title={'Boxing icon has the will for a couple more fights'}
             description={'The highly anticipated world championship fight will take place at 10am and is the second major boxing blockbuster in the nation after 43 years.'}>
 
 
-            <button className='cursor-pointer absolute right-5 top-4'>
+            <button className='cursor-pointer absolute right-5 top-4' onClick={actionsVisibilityHandler}>
               <UilEllipsisH color='#444368' />
             </button>
           </CardFooter>
