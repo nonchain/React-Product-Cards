f31fffdb3ad62cf5a5f5d7c0d648d6248bb50ccc
progali007 <code.progali@gmail.com>
2022-09-05 13:24:39 +0430

    Change some styles

--- news-cards/src/components/CardFooter.jsx
+++ news-cards/src/components/CardFooter.jsx
@@ -4,7 +4,7 @@ const CardFooter = ({children, className, author, title, description}) => {
   return (
     <div className={`w-full flex flex-col absolute bottom-0 left-0 z-20 ${className}`}>
       <span className='text-xs text-zinc-800  font-normal'>{author}</span>
-      <h2 className='mt-5 text-3xl text-zinc-500 font-extralight'>{title}</h2>
+      <h2 className='mt-5 cursor-pointer text-3xl text-zinc-500 font-extralight'>{title}</h2>
       <p className='mt-7 text-[15px] font-normal'>{description}</p>
       {children}
     </div>
