1110e8c2bb11aa88fe4cadbf95707a0756855eb4
progali007 <code.progali@gmail.com>
2022-09-05 05:21:59 +0430

    Responsive the Components

--- gamer-ranking-card/src/App.jsx
+++ gamer-ranking-card/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Card from './components/Card';
 import GamerStatus from './components/GamerStatus';
 import GamerSummary from './components/GamerSummary';
+import GamerDetails from './components/GamerDetails';
 
 import GamerBoyAvatar from './assets/image/gamer_boy.png';
-import GamerDetails from './components/GamerDetails';
+import GamerGirlAvatar from './assets/image/gamer_girl.png';
 
 function App() {
   return (
-    <div className="content">
+    <div className="content grid md:grid-cols-2 gap-5">
       <Card className='grid grid-rows-3 relative card'>
         <GamerSummary
           fullName={'Richard Tyler Blevins'}
@@ -27,6 +28,28 @@ function App() {
           />
 
           <GamerStatus avatarUrl={GamerBoyAvatar} level='13' score='5,321' className='w-full absolute bottom-6' />
+        </div>
+
+      </Card>
+
+      <Card className='grid grid-rows-3 relative card'>
+        <GamerSummary
+          className='row-span-2'
+          fullName={'Tiffany Garcia'}
+          summary={'Tiffany Michelle Herrera (born: March 19, 1988), (née Garcia), more commonly known as Cupquake, is a YouTube gaming commentator, fashion vlogger and an animal lover'} />
+
+        <div className='w-full h-28 bg-blue-500 absolute bottom-0 overflow-hidden additional'>
+          <GamerDetails
+            fullName={'Tiffany Garcia'}
+            group={'iHasCupquake'}
+            role={'Gamer'}
+            joinDate={'2022/5'}
+            city={'Abhar'}
+            country={'Iran'}
+            className='w-full absolute bottom-[108px]'
+          />
+
+          <GamerStatus avatarUrl={GamerGirlAvatar} level='13' score='5,321' className='w-full absolute bottom-6' />
         </div>
 
       </Card>
--- gamer-ranking-card/src/components/GamerSummary.jsx
+++ gamer-ranking-card/src/components/GamerSummary.jsx
@@ -1,23 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-const getWindowWidth = ()=> {
+const getWindowWidth = () => {
   const { innerWidth: width, } = window;
   return width;
 }
 
-const GamerSummary = ({fullName, summary, className}) => {
-  
+const GamerSummary = ({ fullName, summary, className }) => {
+
   const [screenWidth, setScreenWidth] = useState(getWindowWidth());
   let message = screenWidth > 768 ? 'Hover the card for more info' : 'Tap to card for more info';
 
   useEffect(() => {
-    window.addEventListener('resize', ()=> setScreenWidth(getWindowWidth()));
+    window.addEventListener('resize', () => setScreenWidth(getWindowWidth()));
   }, []);
 
   return (
     <div className={`pt-8 pb-5 px-4 md:px-6 flex flex-col ${className}`}>
       <h2 className='text-2xl font-semibold text-zinc-800'>{fullName}</h2>
-      <p className='mt-5'>{summary}</p>
+      <p className='mt-5'>
+        {summary.length > 90 ? `${summary.substring(0, 90)} ...` : summary.substring(0, summary.length)}
+      </p>
       <span className='mt-12 text-right text-xs font-medium'>{message}</span>
     </div>
   )
