55e79752f2131f235d9f5786a7c97b3bf73ca6ea
progali007 <code.progali@gmail.com>
2022-09-05 14:18:15 +0430

    Desined Boxing Card

--- news-cards/src/App.jsx
+++ news-cards/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Card from './components/Card';
 import CardImage from './components/CardImage';
-
-import Boxing from './assets/images/boxing.jpg';
 import CardHeader from './components/CardHeader';
 import CardFooter from './components/CardFooter';
+import { UilEllipsisH } from '@iconscout/react-unicons';
+
+import Boxing from './assets/images/boxing.jpg';
+import CardAction from './components/CardAction';
 
 function App() {
   return (
@@ -18,14 +20,21 @@ function App() {
         </CardHeader>
         <CardImage src={Boxing} />
 
-        <CardFooter 
-        className={'pt-4 pb-5 px-5 bg-white'} 
-        author={'Jane Doe'} 
-        title={'Boxing icon has the will for a couple more fights'} 
-        description={'The highly anticipated world championship fight will take place at 10am and is the second major boxing blockbuster in the nation after 43 years.'}>
-          
-          
-        </CardFooter>
+        <div className='flex flex-col absolute bottom-0 left-0 z-20'>
+          <CardAction iconSize={24} comment={'8'} like={'47'} save={'0'} className={'py-2 bg-green-500 justify-around'}/>
+
+          <CardFooter
+            className={'pt-4 pb-5 px-5 bg-white relative'}
+            author={'Jane Doe'}
+            title={'Boxing icon has the will for a couple more fights'}
+            description={'The highly anticipated world championship fight will take place at 10am and is the second major boxing blockbuster in the nation after 43 years.'}>
+
+
+            <button className='cursor-pointer absolute right-5 top-4'>
+              <UilEllipsisH color='#444368' />
+            </button>
+          </CardFooter>
+        </div>
       </Card>
     </div>
   )
